a865555748bda94463d7f979e769e5cbe2b5988f
Adelina Mihaylova <aggmihaylova@gmail.com>
2019-07-10 11:02:59 +0300

    added simple example

--- src/ManagerTest.java
+++ src/ManagerTest.java
@@ -4,9 +4,13 @@ class ManagerTest {
 
     @Test
     void getFirstName() {
+   // not working
+
         //given
         Manager manager = new Manager("A","B");
         String expectedFirstName = "A";
+
+        // when
 
         //then
         assert(expectedFirstName.equals(manager.getFirstName()));
